a89f8580e64fd2b615a0d420365e6b7a201de962
Anish <anish.appadoo@sdworx.com>
2022-07-12 12:33:02 +0400

    [figma-tokens] Modified sync-tokens script

--- figma/scripts/sync-tokens.cjs
+++ figma/scripts/sync-tokens.cjs
@@ -68,11 +68,16 @@ function lintCode() {
 
 function stageAndCommitCode() {
   return new Promise((resolve, reject) => {
-    const stageAndCommitCommand = `git add . && git commit -m "[${FIGMA_SYNC_BRANCH}] Synced figma tokens"`;
+    const stageCommand = 'git add .';
+    const commitCommand = `git commit -m "[${FIGMA_SYNC_BRANCH}] Synced figma tokens"`;
 
-    asyncExec(stageAndCommitCommand)
-      .then(() => resolve())
-      .catch((e) => reject(e));
+    asyncExec(stageCommand)
+      .then(() =>
+        asyncExec(commitCommand)
+          .then(() => resolve())
+          .catch((e) => reject('error in stage'))
+      )
+      .catch((e) => reject('error in commit'));
   });
 }
 
